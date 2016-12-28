5223c89ba80882acc4f597661378278eb726d356
susier2016 <susmitarenjth@gmail.com>
2016-12-28 12:36:03 -0600

    random

--- public/chat.js
+++ public/chat.js
@@ -3,7 +3,7 @@ window.onload = function() {
     var messages = [];
     //https://guarded-lowlands-86253.herokuapp.com
     //http://localhost:3700
-    var host = 'http://localhost:3700';
+    var host = 'https://guarded-lowlands-86253.herokuapp.com';
     var socket = io.connect(host);
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
